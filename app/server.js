f5120110ff3c91a504a3a044611e96080544aecf
dannycho7 <dannycho7@gmail.com>
2017-04-29 15:35:57 -0700

    V4.2

--- app/server.js
+++ app/server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const request = require('request');
+const cheerio = require('cheerio');
+
 
 app.listen(PORT, () => {
   console.log("Server listening in on port", PORT);
