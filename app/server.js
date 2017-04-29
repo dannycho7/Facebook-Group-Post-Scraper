dd8ae8e11fdb2494bed3170b76ed0fbf8c7246ab
dannycho7 <dannycho7@gmail.com>
2017-04-29 01:34:18 -0700

    Basic setup V3

---
+++ app/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+// App setup
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server listening in on port", PORT);
+});
