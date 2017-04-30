27d91783d9ec2e15b6e28572a9aa4c920eeaba57
dannycho7 <dannycho7@gmail.com>
2017-04-30 01:15:16 -0700

    content script for json

--- app/user_ext/js/data_content.js
+++ app/user_ext/js/data_content.js
@@ -1,14 +1,15 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
   alert("Received message from background");
   var posts = document.getElementsByClassName('fbUserContent');
-  var returnVal = [];
+  var returnVal = {};
   for(var i in posts) {
     if(isNaN(i) || i == 0){
       // Don't modify returnVal
     } else {
       console.log("Modified index " + i);
-      returnVal.push(posts[i].getElementsByClassName('userContent')[0].children[0].innerHTML);
+      returnVal[i] = (posts[i].getElementsByClassName('userContent')[0].children[0].innerHTML);
     }
   }
+  console.log(returnVal);
   sendResponse(returnVal);
 });
