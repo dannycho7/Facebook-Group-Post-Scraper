3a36fde420727719652a0eb214850b8934e9bf57
dannycho7 <dannycho7@gmail.com>
2017-04-30 00:27:10 -0700

    setup to receive from ext

--- app/server.js
+++ app/server.js
@@ -6,7 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const request = require('request');
 const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
+const util = require('util');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("*", (req, res) => {
+  let $ = cheerio.load(req.body.response);
+  console.log("Response:", util.inspect(req.body.response));
+  res.end();
+});
 
 app.listen(PORT, () => {
   console.log("Server listening in on port", PORT);
